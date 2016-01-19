9d03d2a226430e9da4140a2aa60d7376dc90f7de
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-01-19 23:22:51 +0300

    done ex. 3 (non-recursive, in-place)

--- ch1_arrays_strings/reverse_string/reverse_string/main.cpp
+++ ch1_arrays_strings/reverse_string/reverse_string/main.cpp
@@ -10,21 +10,59 @@
 #include <string>
 using namespace std;
 
-string sreverse(string s);
+string sreverse(string &s);
+void sreverse2(string &s);
+void swap(string &s, int i, int j);
 
 int main() {
     
     string s = "hello";
-    cout << sreverse(s) << endl;
+    sreverse2(s);
+    cout << s << endl;
     return 0;
 }
 
 
-string sreverse(string s) {
+string sreverse(string &s) {
     
     if (s.length() == 0) {
         return "";
     }
     
-    return s.back() + sreverse(s.substr(0, s.length() - 1));
+    string sub = s.substr(0, s.length() - 1);
+    return s.back() + sreverse(sub);
 }
+
+void swap(string &s, int i, int j) {
+    
+    char tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+}
+
+void sreverse2(string &s) {
+    
+    for (int i = 0; i < s.length() / 2; i++) {
+        swap(s, i, (int) s.length() - 1 - i);
+    }
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
