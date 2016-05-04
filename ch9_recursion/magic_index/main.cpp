553e37f5e0c7712ad0a190dd62eaf908b23501a4
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-05-04 20:57:39 +0300

    misc changes

--- ch9_recursion/magic_index/main.cpp
+++ ch9_recursion/magic_index/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-void magic(vector<int> A, int left, int right) {
+void magic(vector<int> &A, int left, int right) {
 
     cout << left << " " << right;
 
@@ -21,8 +21,29 @@ void magic(vector<int> A, int left, int right) {
     }
 
 }
-void magic(vector<int> A) {
+void magic(vector<int> &A) {
      magic(A, 0, (int) A.size() - 1);
+}
+
+int magic2(vector<int> &A, int left, int right) {
+
+
+    if (left > right) {
+        return -1;
+    }
+
+    int mid = (left + right) / 2;
+    if (A[mid] == mid) {
+        return mid;
+    } else if (A[mid] < mid) {
+        return magic2(A, mid + 1, right);
+    } else {
+        return magic2(A, left, mid - 1);
+    }
+
+}
+int magic2(vector<int> &A) {
+    magic2(A, 0, 8);
 }
 
 
@@ -30,6 +51,6 @@ void magic(vector<int> A) {
 int main() {
 
     vector<int> A = {-1, 0, 1, 2, 3, 5, 30, 40, 50};
-    magic(A);
+    cout << magic2(A) << endl;
     return 0;
 }
