2db7c1cfbd40f47d071669c1ed5a205929edf744
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-05-09 14:24:48 +0300

    add stack based on linked list

--- ch2_stack_queue/ch2_stack_queue_java/src/main/java/com/ilyarudyak/java/interview/Stack.java
+++ ch2_stack_queue/ch2_stack_queue_java/src/main/java/com/ilyarudyak/java/interview/Stack.java
@@ -1,9 +1,98 @@
 package com.ilyarudyak.java.interview;
 
+import java.util.NoSuchElementException;
+
 /**
- * Stack based on Linked List
+ * Stack based on Linked List. Code based on this one:
+ * http://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/Stack.java.html
  */
 public class Stack<Item> {
 
+    private Node<Item> first;
+    private int size;
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Stack() {
+        first = null;
+        size = 0;
+    }
+
+    public void push(Item item) {
+
+        Node<Item> newNode = new Node<>();
+        newNode.item = item;
+
+        if (first == null) {
+            newNode.next = null;
+            first = newNode;
+        }
+        else {
+            newNode.next = first;
+            first = newNode;
+        }
+        size++;
+    }
+    public Item pop() {
+        if (isEmpty()) throw new NoSuchElementException("stack is empty");
+        Item item = first.item;
+        first = first.next;
+        size--;
+        return item;
+    }
+
+    // utility functions
+    public boolean isEmpty() {
+        return first == null;
+    }
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        System.out.println("size=" + stack.size());
+
+        while (!stack.isEmpty()){
+            System.out.print(stack.pop() + " ");
+        }
+        System.out.println();
+        System.out.println("size=" + stack.size());
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
