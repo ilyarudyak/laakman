0490600150ac6342fe16ffb6aeec97e58fe18c11
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-05-27 00:17:50 +0300

    done 9.1 staircase (DP solution)

--- ch9_recursion/recursion/Staircase.h
+++ ch9_recursion/recursion/Staircase.h
@@ -6,15 +6,21 @@
 #define RECURSION_STAIRCASE_H
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Staircase {
 public:
 
-    Staircase(int n) : n(n), wayCount(0) { }
+    Staircase(int n) : n(n), wayCount(0) {
+        map.resize((unsigned long) (n + 1), -1);
+    }
 
     void countWays() {
         countWays("", n);
+    }
+    int countWaysDP() {
+        return countWaysDP(n);
     }
 
 
@@ -25,26 +31,46 @@ public:
 private:
     int n;
     int wayCount;
+    vector<int> map;
 
     void countWays(string way, int k) {
+
+        // this idea is from solution - we
+        // may skip 3 cases below
+        if (k < 0) { return; }
+
         if (k == 0) {
             cout << way << endl;
             wayCount++;
             return;
         }
 
-        if (k >= 3) {
+//        if (k >= 3) {
             countWays(way + "3", k - 3);
             countWays(way + "2", k - 2);
             countWays(way + "1", k - 1);
-        } else if (k >= 2) {
-            countWays(way + "2", k - 2);
-            countWays(way + "1", k - 1);
-        } else if (k >= 1) {
-            countWays(way + "1", k - 1);
+//        } else if (k >= 2) {
+//            countWays(way + "2", k - 2);
+//            countWays(way + "1", k - 1);
+//        } else if (k >= 1) {
+//            countWays(way + "1", k - 1);
+//        }
+
+
+    }
+    int countWaysDP(int k) {
+        if (k < 0) {
+            return 0;
+        } else if (k == 0) {
+            return 1;
+        } else if (map[k] > -1) {
+            return map[k];
+        } else {
+            map[k] = countWaysDP(k - 1) +
+                     countWaysDP(k - 2) +
+                     countWaysDP(k - 3);
+            return map[k];
         }
-
-
     }
 
 };
--- ch9_recursion/recursion/main.cpp
+++ ch9_recursion/recursion/main.cpp
@@ -9,9 +9,11 @@ using namespace std;
 int main() {
 
     // test 9.1 staircase
-    Staircase staircase(4);
+    Staircase staircase(5);
     staircase.countWays();
     cout << "# of ways = " << staircase.getWayCount() << endl;
+
+    cout << "# of ways = " << staircase.countWaysDP() << endl;
 
     // test 9.3 magic index
 //    vector<int> v = {-10, -9, -8, 3, 30};
