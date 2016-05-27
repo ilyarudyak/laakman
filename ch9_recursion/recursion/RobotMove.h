b4a6b5f40e6599c7a14a3dc6ba1030325a5f2517
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-05-27 18:25:39 +0300

    done 9.2 robot move

---
+++ ch9_recursion/recursion/RobotMove.h
@@ -0,0 +1,90 @@
+//
+// Created by Ilya Rudyak on 5/27/16.
+//
+
+#ifndef RECURSION_ROBOTMOVE_H
+#define RECURSION_ROBOTMOVE_H
+
+#include <utility>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class RobotMove {
+public:
+
+    RobotMove(const pair<int, int> &finish, pair<int, int> spot) :
+            start(make_pair(0, 0)), finish(finish), countPath(0) {
+        int x = finish.first;
+        int y = finish.second;
+        offLimitSpots.resize((unsigned long) (x + 1), vector<bool>((unsigned long) (y + 1), true));
+        offLimitSpots[spot.first][spot.second] = false;
+    }
+
+    void move() {
+        move(start);
+        cout << "# of paths = " << countPath << endl;
+    }
+
+    void showSpots() {
+        for (int i = 0; i <= finish.first; ++i) {
+            for (int j = 0; j <= finish.second; ++j) {
+                cout << offLimitSpots[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+private:
+    pair<int, int> start;
+    pair<int, int> finish;
+    int countPath;
+    vector<vector<bool>> offLimitSpots;
+
+    bool isFinish(pair<int, int> current) {
+        return  current.first == finish.first &&
+                current.second == finish.second;
+    }
+
+    void move(pair<int, int> cur) {
+
+        cout << cur.first << " " << cur.second << endl;
+        if (isFinish(cur)) {
+            countPath++;
+            return;
+        }
+        if ((cur.first < finish.first) && offLimitSpots[cur.first + 1][cur.second]) {
+            move(make_pair(cur.first + 1, cur.second    ));
+        }
+        if ((cur.second < finish.second) && offLimitSpots[cur.first][cur.second + 1]) {
+            move(make_pair(cur.first    , cur.second + 1));
+        }
+
+
+    }
+
+};
+
+#endif //RECURSION_ROBOTMOVE_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ch9_recursion/recursion/main.cpp
+++ ch9_recursion/recursion/main.cpp
@@ -2,6 +2,7 @@
 #include "Parens.h"
 #include "MagicIndex.h"
 #include "Staircase.h"
+#include "RobotMove.h"
 
 using namespace std;
 
@@ -9,11 +10,16 @@ using namespace std;
 int main() {
 
     // test 9.1 staircase
-    Staircase staircase(5);
-    staircase.countWays();
-    cout << "# of ways = " << staircase.getWayCount() << endl;
+//    Staircase staircase(5);
+//    staircase.countWays();
+//    cout << "# of ways = " << staircase.getWayCount() << endl;
+//
+//    cout << "# of ways = " << staircase.countWaysDP() << endl;
 
-    cout << "# of ways = " << staircase.countWaysDP() << endl;
+    // test 9.2 robot move
+    RobotMove rm(make_pair(2, 2), make_pair(0, 1));
+    rm.showSpots();
+    rm.move();
 
     // test 9.3 magic index
 //    vector<int> v = {-10, -9, -8, 3, 30};
