a57ee99d7c67ef94b24cd61608175894ac72a3c5
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-06-10 15:17:35 +0300

    done largest (correct versio)

--- ch5_bits/bits/src/main/java/NextLargest.java
+++ ch5_bits/bits/src/main/java/NextLargest.java
@@ -17,11 +17,32 @@ public class NextLargest {
         n = toggleBit(n, firstUnSetBit());
         return n;
     }
-
     public int nextLargestFull() {
         int n = N;
-        n = toggleBit(n, firstSetBit());
-        n = toggleBit(n, firstUnSetBit());
+
+        // this is first non-trailing 0
+        // in case of 1010*0*1110 p == 4
+        int p = firstUnSetBit();
+
+        // (1) toggle p
+        // 1010*0*1110 -> 1010*1*1110
+        n = toggleBit(n, p);
+
+        // (2) arrange bits before p (1s to the left and reduce by 1)
+        // 101011110 -> 101010011
+
+        // (2.1) count ones
+        int ones = countOne(p);
+
+        // (2.2) clear bits up to p
+        // 10101*1110 -> 10101*0000
+        n &= ~0 << p;
+
+        // (2.3) set bits
+        // 10101*0000 -> 10101*0011
+        n |= (1 << ones - 1) - 1;
+        System.out.println( Integer.toBinaryString(n) );
+
         return n;
     }
 
@@ -65,14 +86,15 @@ public class NextLargest {
 
     public static void main(String[] args) {
 
-        int N = Integer.parseInt("101001110", 2);
+//        int N = Integer.parseInt("101001110", 2);
+        int N = 13948;
         System.out.print("n=" + N + " ");
         System.out.println( Integer.toBinaryString(N) );
 
         NextLargest nl = new NextLargest(N);
-
-        int p = nl.firstUnSetBit();
-        System.out.println(nl.countOne(p));
+        int next = nl.nextLargestFull();
+        System.out.print("next=" + next + " ");
+        System.out.println( Integer.toBinaryString(next) );
 
 
     }
